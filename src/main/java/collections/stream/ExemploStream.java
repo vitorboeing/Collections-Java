1dddac6e154afdac2b6a6ca40aed09ea7582a633
Vitor Boeing <vitor-bh@outlook.com>
2022-05-03 21:54:58 -0300

    Primeiro commit

--- src/main/java/collections/stream/ExemploStream.java
+++ src/main/java/collections/stream/ExemploStream.java
@@ -19,11 +19,14 @@ public class ExemploStream {
         funcionarios.add(new Funcionario("Vitor", 21, 2.000));
         funcionarios.add(new Funcionario("Vitor", 21, 2.000));
         funcionarios.add(new Funcionario("Vitor", 21, 2.000));
-        funcionarios.add(new Funcionario("Vitor", 21, 2.000));
-        funcionarios.add(new Funcionario("Vitor", 21, 2.000));
+        funcionarios.add(new Funcionario("Vitor", 19, 2.000));
+        funcionarios.add(new Funcionario("Jorge", 55, 2.000));
 
         /// Retorna o Optional contendo o Funcionário mais velho.
         var funcionarioMaisVelho = funcionarios.stream().max(Comparator.comparing(Funcionario::idade));
+
+        /// Retorna o Optional contendo o Funcionário mais novo.
+        var funcionarioMaisNovo = funcionarios.stream().min(Comparator.comparing(Funcionario::idade));
 
 
     }
